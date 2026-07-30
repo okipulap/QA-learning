b75f9390fdb85fda4eacac58368ee5032bbd481d
okipulap <cdeadg@yandex.ru>
2026-07-30 18:46:23 +0300

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,15 @@ repositories {
 }
 
 dependencies {
+    implementation("io.github.bonigarcia:webdrivermanager:6.1.0")
+
+    implementation("org.seleniumhq.selenium:selenium-java:4.39.0")
+
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
